9a2efa3c5791eafbccca5206a304d330f9d0616c
abdul <ahersi960@gmail.com>
2019-01-23 15:45:20 -0700

    Final push

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import os
+import sys
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -30,9 +32,175 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        print("cameinto handle function")
+        current_dir= os.getcwd()
+        updated_dir=current_dir+"/www"
+        dirs=os.listdir(updated_dir)
+        updated_dir1=updated_dir+'/index.html'
+        updated_dir1_css=updated_dir+'/base.css'
+        #updated_dir2=os.path.join(updated_dir,'deep/index.html')
+        updated_dir2=updated_dir+'/index.html'
+        updated_dir2_css=updated_dir+'/deep/deep.css'
+
         self.data = self.request.recv(1024).strip()
+        self.data=self.data.decode("utf-8")
+        print(self.data)
+        a = self.data.splitlines()
+        print("************************")
+        directory=a[0].split()
+        print(updated_dir+directory[1])
+
+        print("Here is wasdfasdfsadhether it is a directoy")
+        print(os.path.exists(updated_dir+directory[1]))
+
+        print(directory[0])
+        print("about to check")
+        if("/.." in directory[1]):
+            print("here is the ...............")
+            self.request.send(b'HTTP/1.1 404 not found\r\n')
+        if(directory[0]!="POST" or directory[0]!="PUT" or directory[0]!="DELETE"):
+            print("11111111111111111111111111111")
+            print(directory[1])
+            if(os.path.exists(updated_dir+directory[1])):
+                print("path does exists")
+                if(".css" in directory[1]):
+                    print("CAME INTO CSS CONDITION")
+                    with open(updated_dir+directory[1],"r") as f:
+                        read_html=f.read()
+                        self.request.send(b'HTTP/1.1 200 OK\n')
+                        self.request.send(b'Content-Type: text/css \n')
+                        self.request.send(b'\n')
+                        self.request.send(bytearray(read_html,'utf-8'))
+                else:
+                    print("CAME INTO INDEX CONDTION")
+                    self.indexPage((updated_dir+directory[1]))
+            else:
+                print("CAME INTO SENDING 404 ERROR")
+                self.request.send(b'HTTP/1.1 404 not found\r\n')
+
+
+            if(".css" in a[0]):
+                print("asdfasfasfsfdsafdsafdsf")
+                with open(updated_dir+directory[1],"r") as f:
+                    read_html=f.read()
+                    self.request.send(b'HTTP/1.1 200 OK\n')
+                    self.request.send(b'Content-Type: text/css \n')
+                    self.request.send(b'\n')
+                    self.request.send(bytearray(read_html,'utf-8'))
+            elif("deep/base.css" in a[0]):
+                print("shouldnt come in here")
+                with open(updated_dir2_css,"r") as f:
+                    read_html=f.read()
+                    self.request.send(b'HTTP/1.1 200 OK\n')
+                    self.request.send(b'Content-Type: text/css \n')
+                    self.request.send(b'\n')
+                    self.request.send(bytearray(read_html,'utf-8'))
+        else:
+            print("22222222222222222222")
+            if("/.." in directory[1]):
+                print("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+                self.request.send(b'HTTP/1.1 404 not found\r\n')
+            else:
+                self.request.send(b'HTTP/1.1 405 Method Not Allowed\r\n')
+
+
+        print("---------------")
+        print("here is a:")
+        print(a)
+        url=""
+        for i in a:
+            print("*&*&*&&&*&*&***&*&*&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+            if("Referer" in i):
+                url=i.split("/")
+        print("the length of the url is :"+str(len(url)))
+        print(url)
+
+        #if len(url)<5:
+        #    print("came in first part")
+        #    self.firstPage(updated_dir1,updated_dir1_css)
+       # else:
+        #    print("came in second part")
+        #    self.secondPage(updated_dir2)
+       
+
+
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        #with open(updated_dir1,"r") as f:
+            #print("------------------")
+         #   read_html=f.read()
+            #print("------------------")
+
+
+        #response = "\HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n"+read_html
+
+
+        
+        #self.request.sendall(bytearray(response,'utf-8'))
+
+    def indexPage(self,updated_dir1):
+        print("CAME INTO FIRSTPAGE FUNCTION")
+        print(updated_dir1)
+        if(".html" in updated_dir1):
+
+            with open(updated_dir1,"r") as f:
+                print("------------------")
+                read_html=f.read()
+                print("------------------")
+            self.request.send(b'HTTP/1.0 200 OK\n')
+            self.request.send(b'Content-Type: text/html \n')
+            self.request.send(b'\n')
+            self.request.send(bytearray(read_html,'utf-8'))
+        else:
+            print(updated_dir1)
+            if(updated_dir1.endswith('/')==False):
+                print("cameinto the right place")
+                self.request.send(b'HTTP/1.1 301 MOVED PERMANENTLY\r\n')
+                self.request.send(b'Location: http://127.0.0.1:8080/deep/index.html\r\n')
+                #self.end_headers()
+
+            else:
+
+                updated_dir1=updated_dir1+'index.html'
+                print("CAADFASDFSADFME IN HERE")
+                print(updated_dir1)
+                with open(updated_dir1,"r") as f:
+                    print("------------------")
+                    read_html=f.read()
+                    print("------------------")
+                self.request.send(b'HTTP/1.0 200 OK\n')
+                self.request.send(b'Content-Type: text/html \n')
+                self.request.send(b'\n')
+                self.request.send(bytearray(read_html,'utf-8'))
+
+            #response = "\HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n"+read_html
+
+            #self.request.sendall(bytearray(response,'utf-8'))
+            #print("should be another css")
+
+            #with open(updated_dir1_css,"r") as f:
+                #print("------------------")
+             #   read_html=f.read()
+                #print("------------------")
+            #response = "\HTTP/1.1 200 OK\r\n Content-Type: text/css\r\n"+read_html
+            #self.request.sendall(bytearray(response,'utf-8'))
+            #print()
+        
+    def secondPage(self,updated_dir2):
+        print("CAME INTO SECONDPAGE FUNCTION")
+        with open(updated_dir2,"r") as f:
+            print(updated_dir2)
+            print(type(updated_dir2))
+            #print("------------------")
+            read_html=f.read()
+            #print("------------------")
+            print(read_html)
+        self.request.send(b'HTTP/1.0 200 OK\n')
+        self.request.send(b'Content-Type: text/html \n')
+        self.request.send(b'\n')
+        self.request.send(bytearray(read_html,'utf-8'))
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +212,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+    
